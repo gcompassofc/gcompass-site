d33edfaef6a1ac9bbb0a62e2e30dee64ef0c10ed
gcompassofc <gcompassofc@github.com>
2026-04-17 01:35:25 -0300

    feat: duplicate forms page to forms-analise

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Imobiliaria from './pages/imobiliaria/Imobiliaria.tsx';
 import ImobiliariaForms from './pages/imobiliaria-forms/ImobiliariaForms.tsx';
+import ImobiliariaFormsAnalise from './pages/imobiliaria-forms-analise/ImobiliariaFormsAnalise.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/imobiliaria" element={<Imobiliaria />} />
         <Route path="/imobiliaria-forms" element={<ImobiliariaForms />} />
+        <Route path="/imobiliaria-forms-analise" element={<ImobiliariaFormsAnalise />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
