4e298024d28180c2cc8b310b49cce01fe8682a29
gcompassofc <gcompassofc@github.com>
2026-04-17 15:20:03 -0300

    fix: type check in framer motion variants

--- src/components/PainPoints.tsx
+++ src/components/PainPoints.tsx
@@ -35,7 +35,7 @@ const containerVariants = {
   }
 };
 
-const itemVariants = {
+const itemVariants: any = {
   hidden: { opacity: 0, y: 30 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } }
 };
